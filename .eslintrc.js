93fd0a2671501824c1d5c2d56d90e9586b35a222
oscar <oscaroceguera@gmail.com>
2019-03-22 02:07:56 -0700

    test pre-commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -19,6 +19,8 @@ module.exports = {
         "react"
     ],
     "rules": {
+        "react/jsx-uses-vars": 2,
+        "react/jsx-uses-react": 2,
         "indent": [
             "error",
             2
